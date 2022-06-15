2f2f8ec3b1ef48c55b2d91eb1e1cc044134fd69c
jesuseunkayode <kayode.jesuseun@hotmail.com>
2022-06-15 09:40:28 +0100

    update the form error

--- valid.js
+++ valid.js
@@ -7,15 +7,28 @@ let passwordError = document.querySelector('#password-error');
 let sumitError = document.querySelector('#sumit-error');
 
 
+
 function validateFirstName() {
     let firstName = document.querySelector("#fname").value;
-  
+    let img = document.querySelector("smoth");
+
     if(firstName.length == 0) {
        firstNameError.innerHTML = "Enter your real name";
        fname.style.border = "1px solid hsl(0, 100%, 74%)";
+       img.style.display = "none";
+       setTimeout(function() {
+        img.style.display = 'block';
+       }, 2000);
         return false;
-    }  else {
-        firstNameError.innerHTML = "great!";
+
+    }  else if(firstName.length <= 3) {
+        firstNameError.innerHTML = "Your name must be atleast 3 long";
+        fname.style.border = "1px solid hsl(0, 100%, 74%)";
+        return false;
+    }
+    
+    else {
+        firstNameError.innerHTML = "Success";
         fname.style.border = "1px solid hsl(154, 59%, 51%)";
     }
         return true;
